9cb292658cf624a153241bbf209a95bd11810e2f
Arun K Bharathan <arun.bharathan@zipstack.com>
2023-01-05 15:56:13 +0530

    ibis timestamp issue demo

---
+++ runme.py
@@ -0,0 +1,23 @@
+import ibis
+from sqlalchemy import text
+from sqlalchemy.exc import ProgrammingError
+
+if __name__ == "__main__":
+    connection = ibis.trino.connect(
+        host="localhost",
+        port=8080,
+        user="test",
+        password="",
+        database="iceberg",
+        schema="dev",
+    )
+
+    with connection.con.engine.connect() as conn:
+        try:
+            result=conn.execute(text("CREATE SCHEMA iceberg.dev WITH (location = 's3a://dev/')"))
+        except ProgrammingError as e:
+            pass
+
+    
+    first = ibis.schema({"a": "timestamp"})
+    connection.create_table("iceberg_table", schema=first)
